2dd410fad95d4540923c7b2020c8cf4157e78e40
Selamawitk <selamawitkinetibeb@gmail.com>
2026-06-13 22:01:16 +0300

    feat: add updateProfile route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,13 @@
-// routes/authRoutes.js
 import express from 'express';
-import { register, signIn } from '../controllers/authController.js';
+import { register, signIn, forgotPassword, resetPassword, updateProfile } from '../controllers/authController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/signin', signIn);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
+router.put('/profile', protect, updateProfile);
 
-export default router; // ✅ ES Module export
+export default router;
