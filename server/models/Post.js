3428828a1841ef82fe0bba69e7b5580aa69afe23
Selamawitk <selamawitkinetibeb@gmail.com>
2026-06-13 22:00:56 +0300

    refactor: update Post model

--- server/models/Post.js
+++ server/models/Post.js
@@ -4,9 +4,9 @@ const postSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     content: { type: String, required: true },
-    user: { // Add this field to link the post to its creator
+    user: { // to link the post to its creator
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User', // This references your User model
+      ref: 'User', //  references User model
       required: true,
     },
   },
