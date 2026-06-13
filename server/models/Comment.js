575dfd2bff2bf3d67e1cd52ad8006546d6b872ee
Selamawitk <selamawitkinetibeb@gmail.com>
2026-06-13 22:00:56 +0300

    refactor: update Comment model

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -16,6 +16,11 @@ const commentSchema = new mongoose.Schema(
       ref: 'Post',
       required: true,
     },
+    parentComment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+      default: null,
+    },
   },
   { timestamps: true }
 );
