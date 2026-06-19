28a76132bc517ca405e4e0952a6b7adafce41372
Selamawitk <selamawitkinetibeb@gmail.com>
2026-06-20 00:07:43 +0300

    refactor: remove unused dateFormatter utility

--- client/src/utils/dateFormatter.js
+++
@@ -1,5 +0,0 @@
-export function formatDate(dateString) {
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  const date = new Date(dateString);
-  return date.toLocaleDateString(undefined, options);
-}
